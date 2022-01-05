f73a97f20e85fc5c032734505fb66c008723a206
Jose Augusto de Oliveira Pins <64021880+JoseAugusto1989@users.noreply.github.com>
2022-01-04 22:20:35 -0300

    Projeto Spring Boot, Java Web

---
+++ src/main/java/controllers/UserController.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import repositories.UserRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping
+    public List<User> findAll() {
+        List<User>  result = repository.findAll();
+        return result;
+    }
+
+    @GetMapping(value = "/{id}")
+    public User findById(@PathVariable Long id) {
+        User  result = repository.findById(id).get();
+        return result;
+    }
+
+    @PostMapping
+    public User insert(@RequestBody User user) {
+        User  result = repository.save(user);
+        return result;
+    }
+}
